3eb72164095c7ff740249f2feef1b92a494f70ce
Ramla Eman <ramla.eman.ra@gmail.com>
2025-06-28 18:49:02 +0500

    pushing the code

---
+++ react/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-700 my-6 mx-auto text-5xl sm:text-8xl ssm:text-6xl'>This is my app</div>
+  )
+}
+
+export default App
